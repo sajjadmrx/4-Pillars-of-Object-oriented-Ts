753084dcba8ac94e4d8fb0574da16a196875bf3f
mrx <sajjadzibaee102@gmail.com>
2022-04-02 17:09:58 +0430

    React.component

--- Abstraction.ts
+++ Abstraction.ts
@@ -1,4 +1,4 @@
-namespace Abstraction_ex1 {
+namespace Abstraction {
 
     abstract class Character {
         constructor(public name: string, public age: number, public damage: number) { }
@@ -24,6 +24,64 @@ namespace Abstraction_ex1 {
     const michael = new Hero("Michael", 30, 15);
     michael.attack();
 
+
+
+}
+
+namespace Abstraction { // React.Component
+    abstract class Component {
+        private _state: any;
+
+
+        constructor(protected props: unknown) { }
+
+        protected get state(): any {
+            return this._state
+        }
+
+        protected setState(state: any) {
+            this._state = state;
+        }
+
+        public abstract render(): void;
+
+    }
+
+
+
+    class button extends Component {
+        constructor(props: unknown) {
+            super(props);
+        }
+        public render(): void {
+            console.log("button");
+        }
+    }
+
+    class Text extends Component {
+        constructor(props: unknown) {
+            super(props);
+            this.setState({
+                text: "hello"
+            })
+        }
+        public render(): void {
+            console.log("text");
+            // this.state = {} //! error
+            this.setState({
+                text: this.state.text + " world"
+            })
+
+        }
+    }
+
+    class Image extends Component {
+        public render(): void {
+            console.log("image");
+        }
+    }
+
+    new Text('').render()
 
 
 }
